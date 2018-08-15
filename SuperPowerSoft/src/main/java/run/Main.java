a18c6436b08010ec836378fcb4647391aef97b23
MrFantaz <dima1_dima2@mail.ru>
2018-08-15 17:48:50 +0300

    first task done creating an interface that is considered maximum

--- SuperPowerSoft/src/main/java/run/Main.java
+++ SuperPowerSoft/src/main/java/run/Main.java
@@ -2,10 +2,15 @@ package run;
 
 
 import filereader.ConvertToUser;
+import util.UtilFromUser;
 
-public class Main {
+public class Main implements UtilFromUser{
+    Main(){
+        System.out.println(getUserWorkTogither(ConvertToUser.getUserArrayList()));
+    }
     public static void main(String[] args) {
         ConvertToUser  convertToUser = new ConvertToUser("input.txt");
         ConvertToUser.getUserArrayList().stream().forEach(System.out::println);
+        new Main();
     }
 }
